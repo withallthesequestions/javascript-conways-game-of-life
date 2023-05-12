8aff64cd56eb8e8b713011f18d789d3fd1257699
Josh Rosario <withallthesequestions@users.noreply.github.com>
2023-05-12 19:23:49 -0400

    Working on cleaner functions

--- script.js
+++ script.js
@@ -1,46 +1,60 @@
-// a. Make a 2D matrix.
-let cellMatrix = [];
-function create2DMatrix(rows, columns) {
+// Assign your matrix dimensions here
+const XVALUE = 3
+const YVALUE = 3
+
+// a. Make a 2D matrix, of specified size.
+let binaryMatrix = [];
+function generateMatrix(rows, columns) {
 	for (let i = 0; i < rows; i++) {
-		cellMatrix[i] = [];
-		let divGrid = document.getElementById("table");
-		divGrid.innerHTML += `<br>`;
+		binaryMatrix[i] = [];
+ 		let divGrid = document.getElementById("matrixTable");
+		divGrid.innerHTML += `<br>`; 
 		for (let j = 0; j < columns; j++) {
-			// b. Assign life/death to each cell randomly
-			cellMatrix[i][j] = Math.round(Math.random());
-			divGrid.innerHTML += `<div class="grid">${cellMatrix[i][j]}</div>`;
+			assignInitialValues(i, j)
+			renderHTMLGrid(i, j) 
 		}
 	}
-	console.table(cellMatrix);
+	console.log("This is the initial grid:");
+	console.table(binaryMatrix);
 }
-create2DMatrix(3, 3);
 
-// c. Check neighbor life status
-function logLifeInNeighborCells(xValue, yValue) {
-	let selectedCell = cellMatrix[xValue][yValue];
+generateMatrix(XVALUE, YVALUE);
+
+// b. Assign life/death value to each cell randomly
+function assignInitialValues(xIndex, yIndex){
+	binaryMatrix[xIndex][yIndex] = Math.round(Math.random());
+}
+// c. Insert matrix into HTML
+function renderHTMLGrid(xIndex, yIndex) {
+	let parentGrid = document.getElementById("matrixTable");
+	parentGrid.innerHTML += `<div class="parentGrid">${binaryMatrix[xIndex][yIndex]}</div>`;
+}
+
+// d. Check neighbor life status
+function logLivingNeighbors(xValue, yValue) {
+	let selectedCell = binaryMatrix[xValue][yValue];
 	let neighborCells = [
-		cellMatrix[yValue - 1][xValue - 1],
-		cellMatrix[yValue - 1][xValue],
-		cellMatrix[yValue - 1][xValue + 1],
-		cellMatrix[yValue][xValue - 1],
-		cellMatrix[yValue][xValue],
-		cellMatrix[yValue][xValue + 1],
-		cellMatrix[yValue + 1][xValue - 1],
-		cellMatrix[yValue + 1][xValue],
-		cellMatrix[yValue + 1][xValue + 1]
+		binaryMatrix[yValue - 1][xValue - 1],
+		binaryMatrix[yValue - 1][xValue],
+		binaryMatrix[yValue - 1][xValue + 1],
+		binaryMatrix[yValue][xValue - 1],
+		binaryMatrix[yValue][xValue],
+		binaryMatrix[yValue][xValue + 1],
+		binaryMatrix[yValue + 1][xValue - 1],
+		binaryMatrix[yValue + 1][xValue],
+		binaryMatrix[yValue + 1][xValue + 1]
 	];
 	const livingNeighborAmount = neighborCells.reduce((a, c) => a + c, 0);
-	console.log("total living neighbors=", livingNeighborAmount);
-	console.log("selected cell status=", selectedCell);
+	console.log("selected cell value=", selectedCell);
+	console.log("number of living neighbors=", livingNeighborAmount);
 	updateCell(selectedCell, livingNeighborAmount);
-
 }
+logLivingNeighbors(1, 1);
 
-logLifeInNeighborCells(1, 1);
-
+// e. Update selected cell according to rules.
 function updateCell(selectedCell, livingNeighborAmount) {
 	if (selectedCell === 1) {
-		console.log("it's 1");
+		console.log("starting state: 1");
 		switch (livingNeighborAmount) {
 			case 0:
 			case 1:
@@ -62,7 +76,7 @@ function updateCell(selectedCell, livingNeighborAmount) {
 				break;
 		}
 	} else {
-		console.log("it's 0");
+		console.log("starting state: 0");
 		switch (livingNeighborAmount) {
 			case 0:
 			case 1:
@@ -75,25 +89,34 @@ function updateCell(selectedCell, livingNeighborAmount) {
 				selectedCell = 1
 				break;
 			case 4:
+			case 5:
+			case 6:
+			case 7:
+			case 8:
 				console.log("dead");
 				selectedCell = 0
 				break;
 		}
 	}
-	console.log("selected cell status=", selectedCell);
+	console.log("updated cell status=", selectedCell);
 }
+
+/* Next: Generate new grid, with empty values, and push the new values in.
+ * 
+ */
+
 /*
-  1. Create a function to update the grid.
-	  a. Loop through 2d array, updating all values.
-  2. Create a function that takes in the two values, and implements the logic of conway's rules.
-	  a. Check if selected cell is dead/alive
-	  b. If cell is dead, then:
-		  if, 3 neighbors are alive, then resurrect it.
-		  else, leave it dead
-	  c. If cell is alive, then:
-		  if 4 or more neighbors are alive, then kill it.
-		  if less than two neighbors are alive, then kill it.
-		  if 2-3 neighbors are alive, then resurrect it.
+1. Create a function to update the grid.
+	a. Loop through 2d array, updating all values.
+2. Create a function that takes in the two values, and implements the logic of conway's rules.
+	a. Check if selected cell is dead/alive
+	b. If cell is dead, then:
+		if, 3 neighbors are alive, then resurrect it.
+		else, leave it dead
+	c. If cell is alive, then:
+		if 4 or more neighbors are alive, then kill it.
+		if less than two neighbors are alive, then kill it.
+		if 2-3 neighbors are alive, then resurrect it.
  */
 
 //setInterval(incrementOneGeneration, 3000);
